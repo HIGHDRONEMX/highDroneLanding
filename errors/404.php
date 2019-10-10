a4140b56c70ce3d9d8baae44a55da72111f8f3ee
sarquamon <sarquamon@gmail.com>
2019-10-10 10:09:57 -0500

    First files had been added

---
+++ errors/404.php
@@ -0,0 +1,16 @@
+<?php
+    include("../header.php");
+?>
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>¡Uy!</h1>
+			</div>
+			<h2>404 - P&aacute;gina no encontrada</h2>
+			<p>La p&aacute;gina que buscas pudo haber sido removida, cambi&oacute; su nombre o est&aacute; temporalmente no disponible.</p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="../index.php">Ir a inicio</a>
+		</div>
+	</div>
+<?php
+    include("../footer.php");
+?>
---
+++ index.php
@@ -0,0 +1,262 @@
+<?php
+include("header.php");
+?>
+<!-- Masthead -->
+<header class="masthead">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center justify-content-center text-center">
+      <div class="col-lg-10 align-self-end">
+        <h1 class="text-uppercase text-white font-weight-bold">
+          El futuro... reimaginado
+        </h1>
+        <h2 class="text-uppercase text-white font-weight-bold">High Drone MX</h2>
+        <hr class="divider my-4" />
+      </div>
+      <div class="col-lg-8 align-self-baseline">
+        <p class="text-white-75 font-weight-light mb-5">¡Volando hacia el futuro!</p>
+        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">M&aacute;s informaci&oacute;n</a>
+      </div>
+    </div>
+  </div>
+</header>
+
+<!-- About Section -->
+<section class="page-section bg-primary" id="about">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-8 text-center">
+        <h2 class="text-white mt-0">
+          ¡Tenemos todo lo que usted necesita!
+        </h2>
+        <hr class="divider light my-4" />
+        <p class="text-white-50 mb-4">
+          ¡Sin rodeos, ni plazos forzosos! Usted contar&aacute; con el apoyo
+          necesario con nuestro personal altamente capacitado para sus
+          proyectos
+        </p>
+        <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">¡Comenzar!</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Services Section -->
+<section class="page-section" id="services">
+  <div class="container">
+    <h2 class="text-center mt-0">Siempre a tu servicio</h2>
+    <hr class="divider my-4" />
+    <div class="row">
+      <div class="col-lg-3 col-md-6 text-center">
+        <div class="mt-5">
+          <i class="fas fa-4x fa-gem text-primary mb-4"></i>
+          <h3 class="h4 mb-2">Nacidos fuertes</h3>
+          <p class="text-muted mb-0">
+            Con los mejores materiales para soportar hasta el entorno mas
+            duro
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 text-center">
+        <div class="mt-5">
+          <i class="fas fa-4x fa-heart text-primary mb-4"></i>
+          <h3 class="h4 mb-2">Hechos con amor</h3>
+          <p class="text-muted mb-0">
+            ¿Realmente es un buen proyecto si no le das amor?
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 text-center">
+        <div class="mt-5">
+          <i class="fas fa-4x fa-tasks text-primary mb-4"></i>
+          <h3 class="h4 mb-2">Al d&iacute;a</h3>
+          <p class="text-muted mb-0">
+            Nuestros proyectos se mantienen actualizados para mantener
+            nuestro alto standar
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 text-center">
+        <div class="mt-5">
+          <i class="fas fa-4x fa-check-double text-primary mb-4"></i>
+          <h3 class="h4 mb-2">Listo para su uso</h3>
+          <p class="text-muted mb-0">
+            ¡Te entregamos tu pedido listo para ser utilizado!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Portfolio Section -->
+<section id="portfolio">
+  <div class="container-fluid p-0">
+    <div class="row no-gutters">
+      <div class="col-lg-4 col-sm-6">
+        <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg">
+          <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="" />
+          <div class="portfolio-box-caption">
+            <div class="project-category text-white-50">
+              Drone de aplicaci&oacute;n
+            </div>
+            <div class="project-name">
+              Toxicity Canary
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
+          <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="" />
+          <div class="portfolio-box-caption">
+            <div class="project-category text-white-50">
+              Drone de entrenamiento
+            </div>
+            <div class="project-name">
+              Tello
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
+          <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="" />
+          <div class="portfolio-box-caption">
+            <div class="project-category text-white-50">
+              Drone de Carreras
+            </div>
+            <div class="project-name">
+              L&iacute;der
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <a class="portfolio-box" href="img/portfolio/fullsize/4.jpg">
+          <img class="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="" />
+          <div class="portfolio-box-caption">
+            <div class="project-category text-white-50">
+              Drone de aplicaci&oacute;n
+            </div>
+            <div class="project-name">
+              Agro+
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
+          <img class="img-fluid" src="img/portfolio/thumbnails/5.jpg" alt="" />
+          <div class="portfolio-box-caption">
+            <div class="project-category text-white-50">
+              Realidad Virtual
+            </div>
+            <div class="project-name">
+              Juegos y experiencia en RV
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
+          <img class="img-fluid" src="img/portfolio/thumbnails/6.jpg" alt="" />
+          <div class="portfolio-box-caption p-3">
+            <div class="project-category text-white-50">
+              Drone de Aplicaci&oacute;n
+            </div>
+            <div class="project-name">
+              Vigilant Drone
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Team Section -->
+<section class="page-section bg-dark text-white" id="Team">
+  <div class="container text-center">
+    <h2 class="mb-4">¡Conoce nuestro equipo!</h2>
+    <hr class="divider my-4" />
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+          <img class="img-fluid rounded-circle mb-3" width="300px" height="300px" src="img/profile/LIOL2.jpg" alt="LIOL" />
+          <h5>Luis Ivan Ortiz Lechuga</h5>
+          <p class="font-weight-light mb-0"> ""</p>
+          <small class="font font-weight-bold">@</small>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+          <img class="img-fluid rounded-circle mb-3" width="300px" height="300px" src="img/profile/JRIV.jpg" alt="JRIV" />
+          <h5>Jaqueline Ramona Islas Valencia</h5>
+          <p class="font-weight-light mb-0"></p>
+          <small class="font font-weight-bold">@</small>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+          <img class="img-fluid rounded-circle mb-3" width="300px" height="300px" src="img/profile/JVF.jpg" alt="JVF" />
+          <h5>Jazmín Villanueva Fernando</h5>
+          <p class="font-weight-light mb-0"> ""</p>
+          <small class="font font-weight-bold">@</small>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+          <img class="img-fluid rounded-circle mb-3" width="300px" height="300px" src="img/profile/sarquamon.jpg" alt="Sarquamon" />
+          <h5>Salomón Piña</h5>
+          <p class="font-weight-light mb-0">Director de departamento de sistemas computacionales</p>
+          <small class="font-weight-bold">@Sarquamon</small>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+          <img class="img-fluid rounded-circle mb-3" width="300px" height="300px" src="img/profile/ELJ.jpg" alt="EJL" />
+          <h5>Edwin Juarez Lobato</h5>
+          <p class="font-weight-light mb-0"> Director de departamento de operaciones</p>
+          <small class="font font-weight-bold">@</small>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--<section class="page-section" style="background-color: black" id="carouselTeam">
+
+</section>-->
+
+<!-- Contact Section -->
+<section class="page-section" id="contact">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-8 text-center">
+        <h2 class="mt-0">¡Hagamos Negocios!</h2>
+        <hr class="divider my-4" />
+        <p class="text-muted mb-5">
+          ¿Listo para iniciar un nuevo proyecto con nosotros? ¡Solo llamanos
+          o mandanos un correo y te responderemos tan pronto como sea
+          posible!
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+        <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+        <div>+52 (222) 523-2977</div>
+      </div>
+      <div class="col-lg-4 mr-auto text-center">
+        <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+
+        <a class="d-block" href="mailto:highdroneveracruz@gmail.com">highdroneveracruz@gmail.com</a>
+      </div>
+    </div>
+  </div>
+</section>
+<?php
+include("footer.php");
+?>
